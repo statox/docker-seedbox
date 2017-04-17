8b430be15db65b49e3f17e7cb6274e07d75f1ee2
statox <adrien.fabre92@gmail.com>
2017-04-17 20:21:23 +0200

    Fix docker compose installation command

--- multimedia.sh
+++ multimedia.sh
@@ -19,7 +19,7 @@ function installDocker() {
 }
 
 function installCompose() {
-    curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-"$(uname -s)"-"$(uname -s)" > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-"$(uname -s)"-"$(uname -a)" > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 }
 
